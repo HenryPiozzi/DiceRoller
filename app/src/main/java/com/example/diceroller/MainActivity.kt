d727bdcc4cb5001fd0dc93a5af6a85ed413a40ea
HenryPiozzi <anaquin2905@gmail.com>
2025-03-20 09:26:41 -0300

    https://github.com/HenryPiozzi/DiceRoller.git

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller.ui.theme.DiceRollerTheme
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun DiceRollerApp(){
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
+    var result = 1
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(painter = painterResource(R.drawable.dice_1),
+              contentDescription = "1"
+        )
+
+        Spacer(modifier = Modifier.height(26.dp))
+
+        Button(onClick = { result = (1..6).random() }){
+            Text(stringResource(R.string.roll))
+        }
+    }
+}
